a38d1abd56ad513ae7f33a4328239f2a7776cb04
Shikhar <shikhark@Work-MacBook.local>
2018-11-02 02:54:56 +0800

    css

--- project/views.py
+++ project/views.py
@@ -5,7 +5,12 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 # Create your views here.
 
-from project.models import Item
+from project.models import Item, Category
 
 class ItemsAllView(ListView):
     model = Item
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
