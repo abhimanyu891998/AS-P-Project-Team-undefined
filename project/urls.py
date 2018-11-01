0fdcc6a80a8ea2e32d0bb6b84f816269cdb84753
Shikhar <shikhark@Work-MacBook.local>
2018-11-01 23:27:43 +0800

    listview

--- project/urls.py
+++ project/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from project import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('orders/', include('project.urls')),
+    path('supplies', views.ItemsAllView.as_view(), name='items'),
 ]
--- project/views.py
+++ project/views.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from django.views.generic.list import ListView
 # Create your views here.
+
+from project.models import Item
+
+class ItemsAllView(ListView):
+    model = Item
