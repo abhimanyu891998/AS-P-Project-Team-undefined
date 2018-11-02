0889c43f54a71ca301a4c3ce8fd9f9300ed3c946
abhimanyu891998 <singhabhimanyu186@gmail.com>
2018-11-02 12:19:59 +0800

    dispatch_list table added

---
+++ project/static/js/dispatch.js
@@ -0,0 +1,14 @@
+var dispatch_button=document.getElementById("dispatch");
+var gen_CSV_button=document.getElementById("genCSV");
+
+dispatch_button.onclick=function(){
+ 
+        
+        alert((document.getElementById("dispatch").value));
+    
+};
+
+
+gen_CSV_button.onclick=function(){
+    
+}
